dfb87aeac5c422b263ea5bcc8156b6f9889e6781
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-10-02 13:33:16 +0200

    Add files via upload

---
+++ sahovska_tabla_broj_crnih_polja/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace sahovska_tabla_broj_crnih_polja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n, cp;
+            n = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+
+            cp = (m * n) / 2;
+
+            Console.WriteLine(cp);
+        }
+    }
+}
